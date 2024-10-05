2db5361f7482ce4bcb3d06e119ade911eb2bb77e
Renoir Boulanger <contribs@renoirboulanger.com>
2024-10-05 17:42:29 -0400

    fix: 🐛 Apology and link to GitHub repo for the blog index without pagination

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -9,6 +9,16 @@
       >
         <blog-list-model-by-year :contents="contents" :q="q" />
       </div>
+      <rb-notice-box v-if="contents.length > 40" variant="info" lang="en">
+        <strong slot="header">Apologies for the long list</strong>
+        <p>Yes, I should have a paginator.</p>
+        <p>
+          It’s planned
+          <a href="https://github.com/renoirb/site/issues/56" target="_blank">
+            renoirb/site#56 <small>(GitHub) </small>
+          </a>
+        </p>
+      </rb-notice-box>
     </div>
   </div>
 </template>
